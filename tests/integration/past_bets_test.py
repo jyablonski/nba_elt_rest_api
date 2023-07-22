4334e25491e0ad110a40d6e39b4e3e20d8e0e281
jacob <jyablonski9@gmail.com>
2023-07-22 17:24:48 -0500

    API Prod Release v1.4.3 (#32)
    ### Description
    PR focused around adding Download CSV functionality for Past Bets 
    
    ## Added
    - Download CSV Functionality for Past Bets
    - Pull Request Template
    
    ## Updated
    - None
    
    ## Deleted
    - None

--- tests/integration/past_bets_test.py
+++ tests/integration/past_bets_test.py
@@ -18,3 +18,24 @@ def test_past_bets_form_get(client_fixture):
     assert bets_response.status_code == 200
     assert username in bets_response.text
     assert "Houston Rockets" in bets_response.text
+
+
+def test_past_bets_form_post_csv_no_permissions(client_fixture):
+    post_response = client_fixture.post("/past_bets")
+
+    assert post_response.status_code == 401
+    assert post_response.json()["detail"] == "Not authenticated"
+
+
+def test_past_bets_form_post_csv(client_fixture):
+    username = "jyablonski"
+
+    login_response = client_fixture.post(
+        "/login", data={"username": username, "password": "password",},
+    )
+
+    bets_post_response = client_fixture.post("/past_bets")
+
+    assert login_response.status_code == 200
+    assert bets_post_response.status_code == 200
+    assert "username,game_date,home_team,home_team_odds," in bets_post_response.text
