85ca65630fb3705b5c102e027d68c3ce811b4529
jyablonski9 <jyablonski9@gmail.com>
2023-09-30 17:21:07 -0500

    remove pipenv reference

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -11,7 +11,6 @@ apt-get -y upgrade
 apt-get -y install --no-install-recommends git
 apt-get -y install --no-install-recommends gcc
 pip install --upgrade pip
-pip install pipenv
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
