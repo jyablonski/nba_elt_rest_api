25237513c3c7b5770ed89d1feb6c42bde2dd1243
jyablonski9 <jyablonski9@gmail.com>
2023-11-29 18:36:41 -0600

    z

--- tests/unit/salt_test.py
+++ tests/unit/salt_test.py
@@ -3,7 +3,7 @@ from src.utils import generate_salt
 
 def test_generate_salt():
     salt = generate_salt()
-    salt2 = generate_salt(32)
+    salt2 = generate_salt(31)
 
     assert len(salt) == 32
-    assert len(salt2) == 32
+    assert len(salt2) == 31
