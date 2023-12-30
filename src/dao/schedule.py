762a2161cff7c390fa535df13dd494b31cfd11a3
jacob <jyablonski9@gmail.com>
2023-12-29 19:13:04 -0800

    API Prod Release v1.7.3 (#48)
    ### Description
    Added Endpoint for Yesterday's Schedule and fixed Tonight's Bets Page
    
    ## Added
    - Yesterday's Schedule Endpoint for Ingestion Script purposes
    
    ## Updated
    - Tonight's Bets Page AGAIN mfer
    - Standardized Link Locations on the Bets Pages
    
    ## Deleted
    - None

--- src/dao/schedule.py
+++ src/dao/schedule.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 from sqlalchemy.orm import Session
 
@@ -18,5 +18,24 @@ def get_schedule(db: Session):
             Schedule.away_moneyline_raw,
         )
         .filter(Schedule.game_date == datetime.now(timezone.utc).date())
+        .all()
+    )
+
+
+def get_yesterdays_schedule(db: Session):
+    yesterday = datetime.now(timezone.utc).date() - timedelta(days=1)
+
+    return (
+        db.query(
+            Schedule.game_date,
+            Schedule.day_name,
+            Schedule.start_time,
+            Schedule.avg_team_rank,
+            Schedule.home_team,
+            Schedule.home_moneyline_raw,
+            Schedule.away_team,
+            Schedule.away_moneyline_raw,
+        )
+        .filter(Schedule.game_date == yesterday)
         .all()
     )
--- tests/integration/schedule_test.py
+++ tests/integration/schedule_test.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta, timezone
+
+
 def test_schedule(client_fixture):
     response = client_fixture.get("/schedule")
     data = response.json()
@@ -15,3 +18,26 @@ def test_schedule(client_fixture):
         "away_team",
         "away_moneyline_raw",
     ]
+    assert data[0]["game_date"] == str(datetime.now(timezone.utc).date())
+
+
+def test_yesterdays_schedule(client_fixture):
+    response = client_fixture.get("/yesterdays_schedule")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 2
+    assert data[0]["home_team"] == "Philadelphia 76ers"
+    assert list(data[0].keys()) == [
+        "game_date",
+        "day_name",
+        "start_time",
+        "avg_team_rank",
+        "home_team",
+        "home_moneyline_raw",
+        "away_team",
+        "away_moneyline_raw",
+    ]
+    assert data[0]["game_date"] == str(
+        datetime.now(timezone.utc).date() - timedelta(days=1)
+    )
