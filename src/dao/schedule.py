f125ca06bff012b757f4b55b71200ef1dc9ac0d8
jyablonski9 <jyablonski9@gmail.com>
2024-01-05 12:31:00 -0600

    s

--- src/dao/schedule.py
+++ src/dao/schedule.py
@@ -5,23 +5,6 @@ from sqlalchemy.orm import Session
 from src.models import Schedule
 
 
-def get_schedule(db: Session):
-    return (
-        db.query(
-            Schedule.game_date,
-            Schedule.day_name,
-            Schedule.start_time,
-            Schedule.avg_team_rank,
-            Schedule.home_team,
-            Schedule.home_moneyline_raw,
-            Schedule.away_team,
-            Schedule.away_moneyline_raw,
-        )
-        .filter(Schedule.game_date == datetime.now(timezone.utc).date())
-        .all()
-    )
-
-
 def get_yesterdays_schedule(db: Session):
     yesterday = datetime.now(timezone.utc).date() - timedelta(days=1)
 
--- src/routers/schedule.py
+++ src/routers/schedule.py
@@ -22,31 +22,3 @@ def read_schedule(date: date | None = None, db: Session = Depends(get_db)):
 def read_yesterdays_schedule(db: Session = Depends(get_db)):
     schedule = get_yesterdays_schedule(db)
     return schedule
-
-
-# def get_reddit_comments(
-#     db: Session, skip: int = 0, limit: int = 250, text_filter: str | None = None
-# ):
-#     query = db.query(RedditComments)
-
-#     if text_filter:
-#         query = (
-#             query.filter(RedditComments.comment.ilike(f"%{text_filter}%"))
-#             .offset(skip)
-#             .limit(limit)
-#         )
-#     else:
-#         query = query.offset(skip).limit(limit)
-
-#     return query.all()
-
-
-# @router.get("/reddit_comments", response_model=list[RedditBase])
-# @cache(expire=900, key_builder=key_builder_no_db)
-# def read_reddit_comments(
-#     skip: int = 0, limit: int = 250, filter: str = None, db: Session = Depends(get_db)
-# ):
-#     reddit_comments = get_reddit_comments(
-#         db, skip=skip, limit=limit, text_filter=filter
-#     )
-#     return reddit_comments
