f9c49cbd3d923ff6f983b9ef007d9f8f3a219f93
jyablonski9 <jyablonski9@gmail.com>
2024-03-05 17:24:16 -0600

    yee

--- tests/integration/settings_test.py
+++ tests/integration/settings_test.py
@@ -8,7 +8,15 @@ def test_settings_get_no_auth(client_fixture):
         assert response.json()["detail"] == "Not authenticated"
 
 
-def test_settings_post_wrong_auth(client_fixture, admin_user):
+def test_settings_get_consumer_auth(client_fixture, consumer_user):
+    username = "test"
+    response = client_fixture.get("/settings")
+
+    assert response.status_code == 200
+    assert f"hello {username}" in response.text
+
+
+def test_settings_get_admin_auth(client_fixture, admin_user):
     username = "test1"
     response = client_fixture.get("/settings")
 
