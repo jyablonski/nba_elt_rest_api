40e0df1591ee3fae98ef35f0aee5d4f2b31f7d96
jyablonski9 <jyablonski9@gmail.com>
2024-03-13 19:17:02 -0500

    mfer

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,19 @@
 import logging
 import sys
 
+# from opensearch_logger import OpenSearchHandler
 
-def create_logger():
+
+def create_logger() -> logging.Logger:
+    """
+    Logger which writes to both stdout as well as an Opensearch Endpoint
+
+    Args:
+        None
+
+    Returns:
+        Logger
+    """
     logger = logging.getLogger()
 
     # create formatters
@@ -11,6 +22,18 @@ def create_logger():
     )
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
+
+    # opensearch_handler = OpenSearchHandler(
+    #     index_name=opensearch_index,
+    #     hosts=[opensearch_endpoint],
+    #     # http_auth=("admin", "admin"),
+    #     http_compress=True,
+    #     use_ssl=False,
+    #     verify_certs=False,
+    #     ssl_assert_hostname=False,
+    #     ssl_show_warn=False,
+    #     flush_frequency=9,
+    # )
     logger.handlers = [stream_handler]
     logger.setLevel(logging.INFO)
 
